ff5a8015b402989d1adbce1d351152c4a5ae2f57
Gustavo de Oliveira Guerino <gustavoguerino@hotmail.com>
2017-11-04 16:07:23 -0300

    Adicionado listas baseadas no usuario (Vendas)

--- src/services/sales-list/sales-list.service.ts
+++ src/services/sales-list/sales-list.service.ts
@@ -1,12 +1,20 @@
 import { Sale } from './../../models/sale/sale.model';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Injectable()
 export class SalesListService {
-    private salesListRef = this.db.list<Sale>('sales-list');
+    private salesListRef = this.salesListRef = this.db.list<Sale>("Visitante");
 
-    constructor(private db: AngularFireDatabase){}
+    constructor(private db: AngularFireDatabase, private afauth: AngularFireAuth){
+      // Write and read permissions to list is set only to user with uid(user) = uid(list)
+      this.afauth.authState.subscribe(user =>{
+          if(user){
+            this.salesListRef = this.db.list<Sale>(user.uid);
+          }
+      });
+    }
 
     getSalesList(){
         return this.salesListRef;
